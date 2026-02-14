949da0f52031bcb8b78928c5bd8f12bad9ecd5a1
Riley <riley.childs@yahoo.com>
2026-02-13 18:15:29 -0700

    FIx bugs regarding web scraping

--- server/models/db.js
+++ server/models/db.js
@@ -1,15 +1,15 @@
 // db.js
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-dotenv.config();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.resolve(__dirname, '../config/.env') });
 
 export async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected successfully!');
   } catch (err) {
     console.error('MongoDB connection error:', err);
