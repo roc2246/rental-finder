e9d6e1effb017a650faa897d0a0a4e0429d48fef
Riley <riley.childs@yahoo.com>
2026-02-13 17:35:52 -0700

    Created pseudocode for chron job

---
+++ server/chron/index.js
@@ -0,0 +1,14 @@
+import chron from 'chron';
+
+export default function initializeChron() {
+  const app = chron();
+
+//  Object with key value pairs of rental listing urls
+
+// every 5 minutes
+    // Loop through urls
+        // If not there then scrape url\
+        // If delisted then delete from db
+
+ 
+}
