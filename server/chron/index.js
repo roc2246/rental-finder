77191fbe4876e89dc7bde75928a564911140acb2
Riley <riley.childs@yahoo.com>
2026-02-16 15:46:26 -0700

    Fixed for loop error on cron job

--- server/chron/index.js
+++ server/chron/index.js
@@ -13,7 +13,7 @@ export default function initializeChron() {
 
   cron.schedule("*/5 * * * *", async () => {
     let scrapedRentals = [];
-    rentalUrls.forEach(async (url) => {
+    Object.keys(rentalUrls).forEach(async (url) => {
       scrapedRentals += await utilities.scrapeRentals(url);
     });
     scrapedRentals.forEach(async (scrapedData) => {
