90d452e166d617a0e395cc0bc00afc59dababd42
Riley <riley.childs@yahoo.com>
2026-02-16 15:26:22 -0700

    Created Chron logic

--- server/chron/index.js
+++ server/chron/index.js
@@ -1,14 +1,25 @@
-import chron from 'chron';
+import chron from "chron";
+import * as utilities from "../utilities/index.js";
+import * as models from "../models/index.js";
 
 export default function initializeChron() {
   const app = chron();
 
-//  Object with key value pairs of rental listing urls
+  //  Object with key value pairs of rental listing urls
+  const rentalUrls = {
+    luxeStay: "luxe-stay.html",
+    quickRent: "quick-rent.html",
+    realtyHub: "realty-hub.html",
+    apartmentFinder: "apartment-finder.html",
+  };
 
-// every 5 minutes
-    // Loop through urls
-        // If not there then scrape url\
-        // If delisted then delete from db
-
- 
+  app.schedule("*/5 * * * *", async () => {
+    let scrapedRentals = [];
+    rentalUrls.forEach(async (url) => {
+      scrapedRentals += await utilities.scrapeRentals(url);
+    });
+    scrapedRentals.forEach(async (scrapedData) => {
+      await models.addRental(scrapedData);
+    });
+  });
 }
