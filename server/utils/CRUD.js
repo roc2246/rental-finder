f583889a2a7a51329dd5f445d7372cc5e48f9015
Riley <riley.childs@yahoo.com>
2026-02-17 17:40:38 -0700

    Updated manageBatchSize to eliminate nested loops

--- server/utils/CRUD.js
+++ server/utils/CRUD.js
@@ -1,15 +1,15 @@
 export async function manageBatchSize(batchSize, data, modelFunct) {
-  const BATCH_SIZE = batchSize;
   let addedCount = 0;
-  for (let i = 0; i < data.length; i += BATCH_SIZE) {
-    const batch = data.slice(i, i + BATCH_SIZE);
-    console.log(batch)
-    const settled = await Promise.allSettled(
-      batch.map((r) => modelFunct(r)),
-    );
-    settled.forEach((res) => {
-      if (res.status === "fulfilled" && res.value) addedCount++;
-    });
+
+  for (let i = 0; i < data.length; i += batchSize) {
+    const batch = data.slice(i, i + batchSize);
+
+    const settled = await Promise.allSettled(batch.map(modelFunct));
+    
+    addedCount += settled.filter(
+      (res) => res.status === "fulfilled" && res.value
+    ).length;
   }
+
   console.log(`Added ${addedCount} rentals to the database.`);
 }
