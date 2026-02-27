1db3e8b71b185ad5f46b9870e3327ff1789c2833
Riley <riley.childs@yahoo.com>
2026-02-26 20:47:53 -0700

    Created helper function for fetching listings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
 import "./App.css";
+import React from "react";
+import * as fetchLib from "./fettch-library.js"
 
 function App() {
 
+
+  // Call fetchListings when the component mounts
+  React.useEffect(() => {
+    fetchLib.fetchListings({}, 1, 10, {"price": 1});
+  }, []);
   return (
     <>
       <div>
         {/* Filters (zip code, sort by) */}
         {/* Sections for each rental site and listings */}
-          {/* Each click on a listing will open a new tab */}
+        {/* Each click on a listing will open a new tab */}
       </div>
     </>
   );
