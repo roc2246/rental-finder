42afea9916edc658cbbd8b7ffe1da070d5baf6b3
Riley <riley.childs@yahoo.com>
2026-03-03 17:38:24 -0700

    Fixed proxy and 'process is nto defined' error

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api":
+        process.env.NODE_ENV === "development"
+          ? "http://localhost:3000"
+          : "backend url", // your backend URL
+    },
+  },
 })
