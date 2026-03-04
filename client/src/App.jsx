ae3e7597bdd45b95775bf8656c5d7d2753523447
Riley <riley.childs@yahoo.com>
2026-03-03 18:38:02 -0700

    Created filder controlls

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,44 @@
 import "./App.css";
 import React from "react";
-import * as fetchLib from "./js/fetch-library.js"
+import * as fetchLib from "./js/fetch-library.js";
 
 function App() {
+  const [price, setPrice] = React.useState([]);
+  const [rating, setRating] = React.useState([]);
 
-
-  // Call fetchListings when the component mounts
   React.useEffect(() => {
-    fetchLib.fetchListings({}, 1, 10, {"price": 1});
+    // Fetch price and rating data from the server
+    async function loadData() {
+      try {
+        const data = await fetchLib.fetchListings();
+        setPrice(data.price);
+        setRating(data.rating);
+      } catch (err) {
+        console.error("failed to load listings", err);
+      }
+    }
+
+    loadData();
   }, []);
+
   return (
     <>
       <div>
         {/* Filters (zip code, sort by) */}
+        <section className="filters">
+          <label>
+            Zip Code:
+            <input type="text" placeholder="Enter zip code" />
+          </label>
+          <label>
+            Sort By:
+            <select>
+              <option value="price">{price}</option>
+              <option value="rating">{rating}</option>
+            </select>
+          </label>
+        </section>
+
         {/* Sections for each rental site and listings */}
         {/* Each click on a listing will open a new tab */}
       </div>
